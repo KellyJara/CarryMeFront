688acdc59bbb447d69e681730af63ab823f18d73
KellyJara <vaneorozco24@hotmail.com>
2024-11-21 21:09:42 +0100

    Front end part 1

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,38 @@
-# Carry Me
+# Carryme
+This is the official website of Carry Me
+
+## Author
+
+Kelly Vanessa Jaramillo Orozco
+
+## Features
+
+- Fully responsive design.
+- Usage of Bootstrap components (buttons, forms, navigation, etc.).
+- Custom styles to fit the specific requirements of the project.
+- Cross-browser compatibility.
+
+## Technologies
+
+- **HTML5**: Page structure.
+- **CSS3**: Styles and layout.
+- **Bootstrap**: Frontend framework for creating responsive layouts.
+- **JavaScript**: Interactive functionality.
+
+
+## Requirements
+
+- A modern **web browser** (Chrome, Firefox, Safari, Edge).
+- A **code editor** (Visual Studio Code, Sublime Text, etc.) is recommended.
+
+## Installation
+
+Follow these steps to install and run the project locally.
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/KellyJara/CarryMeFront.git
+
+
+
---
+++ Router/allRoutes.js
@@ -0,0 +1,19 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Home", "/pages/home.html"),
+    new Route("/usersignin","user's sign in","/pages/userauth/usersignin.html"),
+    new Route("/usersignup", "User's sign up", "/pages/userauth/usersignup.html","/js/auth/usersignup.js"),
+    new Route("/carriersignin","carrier's sign in","/pages/carrierauth/carriersignin.html"),
+    new Route("/carriersignup", "Carrier's sign up", "/pages/carrierauth/carriersignup.html","/js/auth/carriersignup.js"),
+    new Route("/useraccount","User's account","/pages/userauth/useraccount.html"),
+    new Route("/carrieraccount","Carrier's account","/pages/carrierauth/carrieraccount.html"),
+    new Route("/carriereditinfo","Carrier's edit info","/pages/carrierauth/carriereditinfo.html"),
+    new Route("/usereditinfo","User's edit info","/pages/userauth/usereditinfo.html"),
+    new Route("/photos","Photos","/pages/photos.html"),
+   ];
+    
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Carry Me";
